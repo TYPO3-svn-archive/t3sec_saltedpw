744e00255ac22cb6915f1ee47845044fb944b265
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:42:05 +0000

    * documenting priority value
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14397 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,7 +18,7 @@ t3lib_extMgm::addService(
 		'description' => '',
 		'subtype' => 'authUserFE',
 		'available' => TRUE,
-		'priority' => 60,
+		'priority' => 60, // must be higher than tx_sv_auth (50) and lower than OpenID (75)
 		'quality' => 50,
 		'os' => '',
 		'exec' => '',
