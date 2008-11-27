492a09490976f9b9b3a5d572ddc9f509526ba6c6
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:47:00 +0000

    * shortening ressource of form eval function class
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14409 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 	// Make sure that we are executed only in TYPO3 context
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_t3secsaltedpw_salted'] = t3lib_extMgm::extPath($_EXTKEY, 'res/eval/class.tx_t3secsaltedpw_salted.php');
+$TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_t3secsaltedpw_salted'] = 'EXT:' .  $_EXTKEY . '/res/eval/class.tx_t3secsaltedpw_salted.php';
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/felogin/pi1/class.tx_felogin_pi1.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_felogin_pi1.php');
 
 if ($GLOBALS['TYPO3_LOADED_EXT']['feuser_admin']) {
