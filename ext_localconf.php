54223251825ce87f9c416a017e1b959ab0a42213
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:51:31 +0000

    * Add extension manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14422 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,7 @@ if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
 $TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_t3secsaltedpw_salted'] = 'EXT:' .  $_EXTKEY . '/res/eval/class.tx_t3secsaltedpw_salted.php';
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/felogin/pi1/class.tx_felogin_pi1.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_felogin_pi1.php');
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/install/mod/class.tx_install.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_install.php');
 
 if ($GLOBALS['TYPO3_LOADED_EXT']['feuser_admin']) {
 	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['media/scripts/fe_adminLib.inc'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/ux_fe_adminLib.inc');
