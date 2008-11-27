dfc16f0fb355bc4fa0fe547cf2cbf26d80b88d82
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:31:55 +0000

    * modifying service - service uses Portable PHP Hashing Framework for authentication
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14368 735d13b6-9817-0410-8766-e36946ffe9aa

--- sv1/class.tx_t3secsaltedpw_sv1.php
+++ sv1/class.tx_t3secsaltedpw_sv1.php
@@ -32,10 +32,11 @@
  * @author	Marcus Krause <marcus#exp2008@t3sec.info>
  */
 
+require_once t3lib_extMgm::extPath('t3sec_saltedpw').'res/lib/class.tx_t3secsaltedpw_phpass.php';
 require_once t3lib_extMgm::extPath('t3sec_saltedpw').'res/staticlib/class.tx_t3secsaltedpw_div.php';
 
 /**
- * Class implements salted MD5 authentification service.
+ * Class implements Portable PHP Hashing Framework authentication service.
  *
  * @author  	Marcus Krause <marcus#exp2008@t3sec.info>
  *
@@ -55,28 +56,76 @@ class tx_t3secsaltedpw_sv1 extends tx_sv_authbase {
 	 *
 	 * @access  public
 	 * @param   array     Array containing FE user data of the logged user.
-	 * @return  int       true - the service was able to authenticate the user
-	 * 					  false - false - this service was the right one to authenticate the user but it failed
-	 * 					  100 - just go on. User is not authenticated but there is still no reason to stop
+	 * @return  mixed     boolean false - false - this service was the right one to authenticate the user but it failed
+	 * 					  integer 100 - just go on. User is not authenticated but there is still no reason to stop
+	 *                    integer 200 - the service was able to authenticate the user
 	 */
 	public function authUser($user)	{
 		$OK = 100;
 		$login = $GLOBALS['TSFE']->fe_user->getLoginFormData();
+		$extConf = tx_t3secsaltedpw_div::returnExtConf( $this->extKey );
+		$objPHPass = new tx_t3secsaltedpw_phpass();
 
-		$salt = tx_t3secsaltedpw_div::getSaltByPasswdString( $user['password'] );
-		if (!empty($salt)) {
-			$cmp = tx_t3secsaltedpw_div::salt($login['uident'], $salt);
-			if ($cmp == $user['password']) {
-				$OK = 200;
-				t3lib_div::devLog('Password ok :)', $this->extKey, 1);
-			} else {
-				$OK = false;
-				t3lib_div::devLog('Authentication failed - "'.$cmp.'" is the wrong password', $this->extKey, 2);
+		$validPasswd = false;
+
+			// we process only valid passwords inserted by Portable PHP password hashing framework
+		if (0 == strcmp(substr($user['password'], 0, 3), '$P$')) {
+			$validPasswd = $objPHPass->checkPassword($login['uident'], $user['password']);
+				// test if password needs hash update due to change of hash count value
+			if ($objPHPass->isHashUpdateNeeded($user['password'])) {
+					$this->updatePassword(intval($user['uid']));
 			}
+		} 	// we process also clear-text, md5 and passwords updated by Portable PHP password hashing framework
+		else if (0 == intval($extConf['forcePHPasswd'])) {
+			if (0 == strcmp(substr($user['password'], 0, 4), 'M$P$')) {
+				$validPasswd = $objPHPass->checkPassword(md5($login['uident']), substr($user['password'], 1));
+					// test if password needs to be updated
+				if ($validPasswd && 1 == intval($extConf['updatePasswd'])) {
+					$this->updatePassword(intval($user['uid']), array( 'password' => $objPHPass->getHashedPassword($login['uident'])));
+				}
+			} else if (0 == strcmp(substr($user['password'], 0, 4), 'C$P$')) {
+				$validPasswd = $objPHPass->checkPassword($login['uident'], substr($user['password'], 1));
+					// test if password needs to be updated
+				if ($validPasswd && 1 == intval($extConf['updatePasswd'])) {
+					$this->updatePassword(intval($user['uid']), array( 'password' => $objPHPass->getHashedPassword($login['uident'])));
+				}
+			} else if (preg_match('/[0-9abcdef]{32,32}/', $user['password'])) {
+				$validPasswd = (0 == strcmp(md5($login['uident']), $user['password']) ? true : false);
+					// test if password needs to be updated
+				if ($validPasswd && 1 == intval($extConf['updatePasswd'])) {
+					$this->updatePassword(intval($user['uid']), array( 'password' => $objPHPass->getHashedPassword($login['uident'])));
+				}
+			} else {
+				$validPasswd = (0 == strcmp(md5($login['uident']), $user['password']) ? true : false);
+					// test if password needs to be updated
+				if ($validPasswd && 1 == intval($extConf['updatePasswd'])) {
+					$this->updatePassword(intval($user['uid']), array( 'password' => $objPHPass->getHashedPassword($login['uident'])));
+				}
+			}
+		}
+
+		if ($validPasswd) {
+			$OK = 200;
+			t3lib_div::devLog('Authentication successful for user with uid ' . $user['uid'], $this->extKey, 0);
+		} else if (!$validPasswd && 1 == intval($extConf['onlyAuthService'])) {
+			$OK = false;
+			t3lib_div::devLog('Authentication failed - "' . $cmp . '" is the wrong password for user with uid ' . $user['uid'], $this->extKey, 2);
 		} else {
-			t3lib_div::devLog('No salted password - authentication not possible', $this->extKey, 1);
+			t3lib_div::devLog('Authentication failed - "' . $cmp . '" is the wrong password for user with uid - skipping this service' . $user['uid'], $this->extKey, 1);
 		}
 		return $OK;
+	}
+
+	/**
+	 * Method updates a fe user record - in this case a new password string will be set.
+	 *
+	 * @access  protected
+	 * @param   integer    $uid           uid of user record that will be updated
+	 * @param   mixed      $updateFields  Field values as key=>value pairs to be updated in database
+	 */
+	protected function updatePassword($uid, $updateFields) {
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery( 'fe_users', 'uid = ' . $uid, $updateFields);
+		t3lib_div::devLog('Automatic password update for user with uid ' . $user['uid'], $this->extKey, 1);
 	}
 }
 
