a09fcd344bf0cc9c32880e8e9e73d58ea6f40dc0
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:54:21 +0000

    * Limit felogin XCLASS usage to TYPO3 versions below 4.2.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14429 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,13 +2,20 @@
 	// Make sure that we are executed only in TYPO3 context
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
+	//form evaluation functions
 $TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_t3secsaltedpw_salted'] = 'EXT:' .  $_EXTKEY . '/res/eval/class.tx_t3secsaltedpw_salted.php';
-#$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/felogin/pi1/class.tx_felogin_pi1.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_felogin_pi1.php');
+
+	// install module
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/install/mod/class.tx_install.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_install.php');
 
-if ($GLOBALS['TYPO3_LOADED_EXT']['feuser_admin']) {
-	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['media/scripts/fe_adminLib.inc'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/ux_fe_adminLib.inc');
+	// sysext:felogin
+if ((t3lib_div::int_from_ver(TYPO3_version) >= 4002000)
+		&& (t3lib_div::int_from_ver(TYPO3_version) < 4002004)) {
+	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/felogin/pi1/class.tx_felogin_pi1.php'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/class.ux_tx_felogin_pi1.php');
 }
+
+	// ext:feuser_admin
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['media/scripts/fe_adminLib.inc'] = t3lib_extMgm::extPath($_EXTKEY, 'res/ux/ux_fe_adminLib.inc');
 
 t3lib_extMgm::addService(
 	't3sec_saltedpw',
