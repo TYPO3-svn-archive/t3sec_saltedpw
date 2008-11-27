c2fb01a53db0d84325474114cd17cfc0a0367c3a
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-27 18:30:42 +0000

    * changing password field length of fe_users table to 34 as hashed passwords of Portable PHP hashing framework are of that length
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/trunk@14364 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,6 @@
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
 t3lib_div::loadTCA('fe_users');
-$TCA['fe_users']['columns']['password']['config']['max'] = 44;
+$TCA['fe_users']['columns']['password']['config']['max'] = 34;
 $TCA['fe_users']['columns']['password']['config']['eval'] = 'nospace,required,tx_t3secsaltedpw_salted,password';
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,3 @@
 CREATE TABLE fe_users (
-	password varchar(44) DEFAULT '' NOT NULL,
+	password varchar(34) DEFAULT '' NOT NULL,
 );
