c6e6434bc8149e2da8e3238f0f774dcb168c8be2
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-18 17:55:09 +0000

    Bug #3365: PHP complains about deprecated call-time pass-by-reference
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/branches/RB-TER-DEV@20483 735d13b6-9817-0410-8766-e36946ffe9aa

--- sv1/class.tx_t3secsaltedpw_sv1.php
+++ sv1/class.tx_t3secsaltedpw_sv1.php
@@ -175,7 +175,7 @@ class tx_t3secsaltedpw_sv1 extends tx_sv_authbase {
 
 			if (!empty($this->login['uident_text'])) {
 				$validPasswd = $this->compareUident(
-									&$user,
+									$user,
 									$this->login);
 			}
 
