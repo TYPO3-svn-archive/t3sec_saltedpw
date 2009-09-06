859b4daf342d23e15bae1570119f6447ca5118e1
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-06 20:24:51 +0000

    code cleanup + proper documentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3sec_saltedpw/branches/TRY-SYSEXT-OOP@24068 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/salts/interfaces/interface.tx_saltedpasswords_salts.php
+++ classes/salts/interfaces/interface.tx_saltedpasswords_salts.php
@@ -1,5 +1,47 @@
 <?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Marcus Krause <marcus#exp2009@t3sec.info>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Contains interface "tx_saltedpasswords_salts" to be used in
+ * classes that provide salted hashing.
+ * 
+ * $Id$
+ */
 
+
+/**
+ * Interface with public methods needed to be implemented
+ * in a salting hashing class.
+ * 
+ * @author      Marcus Krause <marcus#exp2009@t3sec.info>
+ * 
+ * @since   	2009-09-06
+ * @package     TYPO3
+ * @subpackage  tx_saltedpasswords
+ */
 interface tx_saltedpasswords_salts {
 
 
@@ -14,8 +56,9 @@ interface tx_saltedpasswords_salts {
 	/**
 	 * Method determines if a given string is a valid salt
 	 * 
-	 * @param  string  $salt  string to check
-	 * @return boolean        true if it's valid salt, otherwise false
+	 * @access  public
+	 * @param   string   $salt: string to check
+	 * @return  boolean  true if it's valid salt, otherwise false
 	 */
 	public function isValidSalt($salt);
 
@@ -23,8 +66,8 @@ interface tx_saltedpasswords_salts {
 	 * Method creates a salted hash for a given plaintext password
 	 * 
 	 * @access  public
-	 * @param   string  $plaintextPassword  password to create a salted hash from
-	 * @param   string  $salt  optional custom salt to use
+	 * @param   string  $password:  plaintext password to create a salted hash from
+	 * @param   string  $salt:  optional custom salt to use
 	 * @return  string  salted hashed password
 	 */
 	public function getHashedPassword($password, $salt = null);
@@ -34,10 +77,15 @@ interface tx_saltedpasswords_salts {
 	 * a given salted hashed password.
 	 * 
 	 * @access  public
-	 * @param   string   $plainPW: 
-	 * @param   string   $saltedHashPW 
+	 * @param   string   $plainPW:  plain-text password to compare with salted hash
+	 * @param   string   $saltedHashPW:  salted hash to compare plain-text password with
 	 * @return  boolean  true, if plaintext password is correct, otherwise false
 	 */
 	public function checkPassword($plainPW, $saltedHashPW);
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/saltedpasswords/classes/salts/interfaces/interface.tx_saltedpasswords_salts.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/saltedpasswords/classes/salts/interfaces/interface.tx_saltedpasswords_salts.php']);
 }
 ?>
